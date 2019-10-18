b494d1e2e232f29a9d192e68d3df1789369d23e6
oscaros <komuntuoscar@gmail.com>
2019-10-18 21:26:50 +0300

    multiple post working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,30 +10,22 @@ import { Observable } from 'rxjs';
 export class HomePage implements OnInit{
 
   phoneCts = [ 
-    {contact_name:"vivian Kikubo", contact_phone: "0784674523"},
-    {contact_name:"Salim G", contact_phone: "0779 902345"},
-    {contact_name:"Chris BOU", contact_phone: "0798 787970"},
-    {contact_name:"Jessie J", contact_phone: "0756 543432"}
+    {contact_name:"vivian Kikubo", contact_phone: "1784674523"},
+    {contact_name:"Salim G", contact_phone: "2779 902345"},
+    {contact_name:"Chris BOU", contact_phone: "3798 787970"},
+    {contact_name:"Jessie J", contact_phone: "4756 543432"}
+
+    //let me connect other 
+    //which settings 
   ];
  
   constructor(public http: HttpClient) {}
   
 
-  saveToCloud(): Observable<any>{
-    
-    var i: number;
-    for (i = 0; i < this.phoneCts.length; i++) {
+  saveToCloud(contact): Observable<any>{
     let type = "application/json: charset-utf-8";
     let headers = new HttpHeaders({'content-type': type});
-    //let options= new RequestOptions({headers:headers});
-
-      console.log(this.phoneCts[i]);
-
-      var i: number;
-      for (i = 0; i < this.phoneCts.length; i++) {
-       return this.http.post('http://testapps.citi-mall.com/codetest/index.php/restPostController/add_contact', JSON.stringify(this.phoneCts[0]), {headers:headers});
-         }    
-    }
+    return this.http.post('http://testapps.citi-mall.com/codetest/index.php/restPostController/add_contact', JSON.stringify(contact), {headers:headers});
 
   }
 
@@ -43,10 +35,15 @@ export class HomePage implements OnInit{
     // })
     // fetch(this.urlService.url + 'add_contact',{method: 'POST', body: JSON.stringify({contact_name:"Naruto", contact_phone: "0784674523"})})
     //  .then(res=>console.log(res))
-
-    this.saveToCloud().subscribe(res=>{
+    
+    var i: number;
+    for (i = 0; i < this.phoneCts.length; i++) {
+      this.saveToCloud(this.phoneCts[i]).subscribe(res=>{
         console.log(res);//lets execute this and see
       })
+    }
+
+   
 
     // var i: number;
     // for (i = 0; i < this.phoneCts.length; i++) {
